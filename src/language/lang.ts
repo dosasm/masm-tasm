5c402ef5a5864720ae8bb30b0f91316aee9e78b5
lc@kun'sASUS <xsro@foxmail.com>
2020-08-13 16:24:03 +0800

    增加显示ASCII

--- src/language/lang.ts
+++ src/language/lang.ts
@@ -426,6 +426,62 @@ function isNumberStr(str:string) : boolean{
 	}
 	return true;
 }
+const asciiname:string[]=[
+	"NUL"+	  localize("ascii.NUL",  "(NULL)"),
+	 "SOH"+	  localize("ascii.SOH",  "(Start Of Headling)"),
+	 "STX"+	  localize("ascii.STX",  "(Start Of Text)"),
+	 "ETX"+	  localize("ascii.ETX",  "(End Of Text)"),
+	 "EOT"+	  localize("ascii.EOT",  "(End Of Transmission)"),
+	 "ENQ"+	  localize("ascii.ENQ",  "(Enquiry)"),
+	 "ACK"+	  localize("ascii.ACK",  "(Acknowledge)"),
+	 "BEL"+	  localize("ascii.BEL",  "(Bell)"),
+	 "BS "+	  localize("ascii.BS",   "(Backspace)"),
+	 "HT "+	  localize("ascii.HT",   "(Horizontal Tab)"),
+	 "LF/NL"+	  localize("ascii.LFNL","(Line Feed/New Line)"),
+	 "VT "+	  localize("ascii.VT",   "(Vertical Tab)"),
+	 "FF/NP "+  localize("ascii.FFNP", "(Form Feed/New Page)"),
+	 "CR"+	  localize("ascii.CR",   "(Carriage Return)	"),
+	 "SO"+	  localize("ascii.SO",   "(Shift Out)"),
+	 "SI"+	  localize("ascii.SI",   "(Shift In)"),
+	 "DLE"+	  localize("ascii.DLE",  "(Data Link Escape)"),
+	 "DC1/XON"+ localize("ascii.DC1", "(Device Control 1/Transmission On)"),
+	 "DC2"+	  localize("ascii.DC2", "(Device Control 2)"),
+	 "DC3/XOFF"+localize("ascii.DC3","(Device Control 3/Transmission Off)"),
+	 "DC4"+	  localize("ascii.DC4", "(Device Control 4)"),
+	 "NAK"+	  localize("ascii.NAK", "(Negative Acknowledge)"),
+	 "SYN"+	  localize("ascii.SYN", "(Synchronous Idle)"),
+	 "ETB"+	  localize("ascii.ETB", "(End of Transmission Block)	"),
+	 "CAN"+	  localize("ascii.CAN", "(Cancel)"),
+	 "EM"+	  localize("ascii.EM",  "(End of Medium)"),
+	 "SUB"+	  localize("ascii.SUB", "(Substitute)"),
+	 "ESC"+	  localize("ascii.ESC", "(Escape)"),
+	 "FS"+	  localize("ascii.FS", "(File Separator)"),
+	 "GS"+	  localize("ascii.GS", "(Group Separator)"),
+	 "RS"+	  localize("ascii.RS", "(Record Separator)"),
+	 "US"+	  localize("ascii.US", "(Unit Separator)"),
+	            localize("ascii.space","(Space)	"),
+	 "!",      "\"",    "#",    
+	 "$",      "%",     "&",      "'",     "(",    
+	 ")",      "*",     "+",      ",",     "-",    
+	 ".",      "/",     "0",      "1",     "2",    
+	 "3",      "4",     "5",      "6",     "7",    
+	 "8",      "9",     ":",      ";",     "<",    
+	 "=",      ">",     "?",      "@",     "A",    
+	 "B",      "C",     "D",      "E",     "F",    
+	 "G",      "H",     "I",      "J",     "K",    
+	 "L",      "M",     "N",      "O",     "P",    
+	 "Q",      "R",     "S",      "T",     "U",    
+	 "V",      "W",     "X",      "Y",     "Z",    
+	 "[",      "\\",    "]",      "^",     "_",    
+	 "`",      "a",     "b",      "c",     "d",    
+	"e",     "f",    "g",     "h",    "i",    
+	"j",     "k",    "l",     "m",    "n",    
+	"o",     "p",    "q",     "r",    "s",    
+	"t",     "u",    "v",     "w",    "x",    
+	"y",     "z",    "{",     "|",    "}",    
+	"~",     
+	"DEL"+	  localize("ascii.DEL", "(Delete)"),
+]
 function getNumMsg(word:string) {
 	let base : number = word.endsWith('h')? 16 : word.endsWith('q')? 8 : word.endsWith('b')? 2 :  10;
 	var value : number = Number.parseInt(word,base);
@@ -434,6 +490,8 @@ function getNumMsg(word:string) {
 		s += " `HEX`: " + value.toString(16) + "H\n\n";
 		s += " `OCT`: " + value.toString(8) + "Q\n\n";
 		s += " `BIN`: " + value.toString(2) + "B\n\n";
+	let a=asciiname[value]
+	if (a)	s += " `ASCII`: " + a;
 	return s;
 }
 function findProc(name:string) : Procedure | undefined{
