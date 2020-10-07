53a1dfc03fb3498a0f71f2794ad7ca46dfee34b7
lc@kun'sASUS <xsro@foxmail.com>
2020-10-07 16:00:12 +0800

    improve the auto mode the part of using dosbox after msdos

--- src/configration.ts
+++ src/configration.ts
@@ -107,8 +107,9 @@ export class Config {
         let command: string = ' ';
         switch (this._BOXrun) {
             case "keep": command = ' '; break;
-            case "exit after run": command = 'exit'; break;
-            case "pause then exit after run": command = 'pause \n exit'; break;
+            case "exit": command = '\n exit'; break;
+            case "pause":
+            default: command = '\n pause \n exit'; break;
         }
         return command;
     }
@@ -120,7 +121,8 @@ export class Config {
         switch (this._BOXrun) {
             case "keep": param = 'k'; break;
             case "exit": param = 'e'; break;
-            case "pause": param = 'p'; break;
+            case "pause":
+            default: param = 'p'; break;
         }
         return param;
     }
