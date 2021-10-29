f8fdd664b106632482f6a6e501f5be5be11cb714
Chen Liu <xsro@foxmail.com>
2021-10-29 20:41:21 +0800

    :bug: use TextDecoder to replace Uint8array toString method

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,18 @@
 import * as vscode from 'vscode';
+import { actionMessage, localize } from './i18n';
+import * as conf from './configuration';
 
 class Logger {
     outputChannel: vscode.OutputChannel = vscode.window.createOutputChannel('masm-tasm');
     log = console.log;
     warn = console.warn;
     error = console.error;
+    localize = localize;
+    actionLog(act: conf.actionType, uri: vscode.Uri) {
+        const message = actionMessage(act, uri.fsPath, conf.extConf.emulator, conf.extConf.asmType);
+        this.channel('\n' + message + '\n').show();
+    }
+
     channel(value: string) {
         this.outputChannel.append(value);
         return this.outputChannel;
--- src/web/ASM.ts
+++ src/web/ASM.ts
@@ -6,6 +6,7 @@ import * as statusBar from '../ASM/statusBar';
 import * as Diag from '../diagnose/main';
 import * as conf from '../utils/configuration';
 import { messageCollector } from '../diagnose/messageCollector';
+import { logger } from '../utils/logger';
 
 const fs = vscode.workspace.fs;
 
@@ -37,13 +38,8 @@ export async function activate(context: vscode.ExtensionContext) {
     const vscode_dosbox = vscode.extensions.getExtension('xsro.vscode-dosbox');
     const api: API = await vscode_dosbox?.activate();
 
-    const assemblyToolsFolder = vscode.Uri.joinPath(context.globalStorageUri, conf.extConf.asmType);
-    const seperateSpaceFolder = vscode.Uri.joinPath(context.globalStorageUri, "workspace");
-
-    async function singleFileMode(type: actionType, _uri: vscode.Uri): Promise<AsmResult> {
-
-        await fs.createDirectory(assemblyToolsFolder);
-
+    async function singleFileMode(act: actionType, _uri: vscode.Uri): Promise<AsmResult> {
+        logger.actionLog(act, _uri);
         const actions: ACTIONS | undefined = vscode.workspace.getConfiguration('masmtasm').get('ASM.actions');
         if (actions === undefined) {
             throw new Error("configurate `masmtasm.ASM.actions` first");
@@ -59,13 +55,12 @@ export async function activate(context: vscode.ExtensionContext) {
         const bundle = await fs.readFile(bundlePath);
 
         let result = "<should-not-return>";
-        const uri = vscode.Uri.joinPath(seperateSpaceFolder, ("test" + path.extname(_uri.fsPath)).toUpperCase());
-        await fs.copy(_uri, uri);
-        const fileInfo = path.parse(uri.fsPath);
+
+        const fileInfo = path.parse(_uri.fsPath);
 
         if (conf.extConf.emulator === conf.DosEmulatorType.jsdos) {
             await api.jsdos.jszip.loadAsync(bundle);
-            api.jsdos.jszip.file('code/' + fileInfo.base, doc.getText());
+            api.jsdos.jszip.file('code/test' + fileInfo.ext, doc.getText());
             const autoexec = [
                 `mount c .`,
                 `mount d ./code`,
@@ -74,22 +69,22 @@ export async function activate(context: vscode.ExtensionContext) {
             ];
             function cb(val: string) {
                 const r = val
-                    .replace("${file}", fileInfo.base)
-                    .replace("${filename}", fileInfo.name);
+                    .replace("${file}", 'test' + fileInfo.ext)
+                    .replace("${filename}", 'test');
                 if (val.startsWith('>')) {
                     return r.replace(">", "");
                 }
                 return r;
             }
-            if (type === actionType.run) {
+            if (act === actionType.run) {
                 autoexec.push(...action.run.map(cb));
             }
-            if (type === actionType.debug) {
+            if (act === actionType.debug) {
                 autoexec.push(...action.debug.map(cb));
             }
             api.jsdos.updateAutoexec(autoexec);
             const webview = await api.jsdos.runInWebview();
-            if (type !== actionType.open) {
+            if (act !== actionType.open) {
                 const [hook, promise] = messageCollector();
                 webview.onDidReceiveMessage(e => {
                     switch (e.command) {
